13bb7a495f8380aa718d845c66f8976c526b3a7a
Don <donabrams@gmail.com>
2011-11-30 09:20:55 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tickler::Application.config.secret_token = 'ca842e8999bdf488e45db12091ed78c1e9465c39f2c238269993808e1d2c54b4a71df874ac074f5126e2ce17d35db7fe7c020bb7b03fc50d064cb0ccbda1f88d'
